77b799ba3ff55cd7721ff30b341b6e4d0a404f42
Igor Prochazka <igor.prochazka@gmail.com>
2015-03-14 23:28:38 +0100

    URL routing and stub home view

--- estupefaktum/urls.py
+++ estupefaktum/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'estupefaktum.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'web.views.home', name='home'),
+    url(r'^recent$', 'web.views.recent', name='recent'),
+    url(r'^contributors$', 'web.views.contributors', name='contributors'),
+    url(r'^submit$', 'web.views.submit', name='submit'),
+    url(r'^login$', 'web.views.login', name='login'),
+    url(r'^signup$', 'web.views.signup', name='signup'),
+    url(r'^logout$', 'web.views.logout', name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- web/views.py
+++ web/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {'word': 'dummy'}
+    return render(request, 'home.html', context)
+
