3165402f7e5d22fb79ed4b6c25bda5eb0b5520ac
Igor Prochazka <igor.prochazka@gmail.com>
2015-03-22 07:36:44 +0100

    Stumb submit page, as example of login_required

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from web.models import Word, Date
 from web import core
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     try:
@@ -15,3 +16,8 @@ def home(request):
 def recent(request):
     words = Word.objects.filter(selected=True).order_by('-its_date').all()
     return render(request, 'recent.html', {'words': words})
+    
+@login_required
+def submit(request):
+    return render(request, 'submit.html', {})
+
