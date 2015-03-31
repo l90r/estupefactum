1b780d36ad58749d5b06ac372f08b5638d89e85d
Igor Prochazka <igor.prochazka@gmail.com>
2015-03-31 11:06:22 +0200

    Introduce form_body template and inclusion tag

--- web/templatetags/bootstrap_form.py
+++ web/templatetags/bootstrap_form.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter()
 def form_control(field):
 	return field.as_widget(attrs={'class': 'form-control'})
+
+@register.inclusion_tag('_form_body.html')
+def form_body(form):
+	return {'form': form}
